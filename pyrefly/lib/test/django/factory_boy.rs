752af23bb7976c7c3b754c64ccd59ecd7c28d8f5
Zeina Migeed <migeedz@meta.com>
2026-04-30 09:17:02 -0700

    Add failing tests for factory-boy build, create_batch, and attribute access
    Summary: Add test cases for build(), create_batch(), and attribute access on factory-created objects. All are marked with bug since pyrefly currently infers Unknown instead of the model type, and reports a false-positive bad-override on the inner Meta class (https://github.com/facebook/pyrefly/issues/3214).
    
    Reviewed By: samwgoldman
    
    Differential Revision: D102416441
    
    fbshipit-source-id: d042051ca9e3334cefeaf837fe3fe7a37094acab

--- pyrefly/lib/test/django/factory_boy.rs
+++ pyrefly/lib/test/django/factory_boy.rs
@@ -29,3 +29,64 @@ user = UserFactory.create()
 assert_type(user, User)  # E: assert_type(Unknown, User) failed
 "#,
 );
+
+factory_boy_testcase!(
+    bug = "https://github.com/facebook/pyrefly/issues/3214",
+    test_build_returns_model,
+    r#"
+from typing import assert_type
+
+from django.db import models
+from factory.django import DjangoModelFactory
+
+class User(models.Model):
+    username = models.CharField(max_length=150)
+
+class UserFactory(DjangoModelFactory):
+    class Meta:  # E: Class member `UserFactory.Meta` overrides parent class `DjangoModelFactory` in an inconsistent manner
+        model = User
+
+user = UserFactory.build()
+assert_type(user, User)  # E: assert_type(Unknown, User) failed
+"#,
+);
+
+factory_boy_testcase!(
+    bug = "https://github.com/facebook/pyrefly/issues/3214",
+    test_create_batch_returns_list,
+    r#"
+from typing import assert_type
+
+from django.db import models
+from factory.django import DjangoModelFactory
+
+class User(models.Model):
+    username = models.CharField(max_length=150)
+
+class UserFactory(DjangoModelFactory):
+    class Meta:  # E: Class member `UserFactory.Meta` overrides parent class `DjangoModelFactory` in an inconsistent manner
+        model = User
+
+users = UserFactory.create_batch(3)
+assert_type(users, list[User])  # E: assert_type(list[Unknown], list[User]) failed
+"#,
+);
+
+factory_boy_testcase!(
+    bug = "https://github.com/facebook/pyrefly/issues/3214",
+    test_model_attribute_access,
+    r#"
+from django.db import models
+from factory.django import DjangoModelFactory
+
+class Document(models.Model):
+    title = models.CharField(max_length=200)
+
+class DocumentFactory(DjangoModelFactory):
+    class Meta:  # E: Class member `DocumentFactory.Meta` overrides parent class `DjangoModelFactory` in an inconsistent manner
+        model = Document
+
+doc = DocumentFactory.create()
+title = doc.title
+"#,
+);
